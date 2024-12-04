8271c0e7e8d0add44cd39c732d45223f8b847999
imran <zunaeede@gmail.com>
2024-12-04 17:12:18 +0600

    main - setup the route and the error page.

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,30 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import MainOutlet from './routes/MainOutlet';
+import Error from './pages/Error';
+import App from './App';
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <MainOutlet />,
+    errorElement: <Error />,
+    children: [
+      {
+        path: '/',
+        element: <App/>,
+      },
+    ],
+  },
+]);
+
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider  router={router}/>
   </StrictMode>,
 )
---
+++ src/pages/Error.jsx
@@ -0,0 +1,12 @@
+
+function Error() {
+  return (
+    <div>
+      <h1 className="font-bold text-4xl text-red-600">
+        You Got Error Boy..
+      </h1>
+    </div>
+  )
+}
+
+export default Error
