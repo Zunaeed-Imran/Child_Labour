d3806f87d0fdb54bd649f87b6605a0ef0defd34a
imran <zunaeede@gmail.com>
2024-12-05 16:30:45 +0600

    main - working on hero section.

--- src/layout/Hero.jsx
+++ src/layout/Hero.jsx
@@ -2,10 +2,12 @@
 
 function Hero() {
   return (
-    <div>
-      
+    <div className="bg-[#FFFFFF] pt-20">
+      <div className="">
+        <h1 className="text-5xl font-bold text-black">Child labour</h1>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Hero
