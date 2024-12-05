15d48e7ef8a640a02da8b2216ac1cc658b3cbf0a
imran <zunaeede@gmail.com>
2024-12-05 16:32:30 +0600

    main - padding.

--- src/layout/Hero.jsx
+++ src/layout/Hero.jsx
@@ -4,7 +4,7 @@ function Hero() {
   return (
     <div className="bg-[#FFFFFF] pt-20">
       <div className="">
-        <h1 className="text-5xl font-bold text-black">Child labour</h1>
+        <h1 className="text-5xl font-bold text-black text-left pl-14">Child labour</h1>
       </div>
     </div>
   );
