3d158d5730a9e909515be33b98abdd05796ae35d
Zunaeed-Imran <zunaeede@gmail.com>
2024-12-06 09:35:04 +0600

    main - text make black.

--- src/layout/Hero.jsx
+++ src/layout/Hero.jsx
@@ -20,14 +20,14 @@ function Hero() {
           <h1 className="text-5xl font-bold text-black">
             What is child labour?
           </h1>
-          <p>
+          <p className="text-black">
             Child labour is a global issue that prevents children from
             fulfilling their potential. Child labour is a violation of
             children’s rights – the work can harm them mentally or physically,
             expose them to hazardous situations or stop them from going to
             school.
           </p>
-          <p>
+          <p className="text-black">
             Not all work done by children is classed as child labour. There is
             not an issue when a child is working to progress their life skills
             without compromising their health, their well-being or their
@@ -35,26 +35,26 @@ function Hero() {
             small jobs to earn pocket money or gain work experience. This work
             prepares them for their adult working life.
           </p>
-          <p>
+          <p className="text-black">
             But around the world, millions of children do work in child labour.
             They work in fields, in factories, down mines, as servants or maids,
             or selling goods in the street or at markets. Girls are more likely
             than boys to do domestic work, such as cleaning, making food and
             serving.
           </p>
-          <p>
+          <p className="text-black">
             The work that children can be forced to do might also be illegal –
             such as working with drug gangs or in prostitution. Many children
             may get no money for the work they do but they will get food and a
             place to sleep. If they become sick or injured, most children will
             not get any money for missing work.
           </p>
-          <p>
+          <p className="text-black">
             All these forms of child labour can prevent a child from going to
             school and limit their opportunity to fulfil their potential. Many
             child labourers never go to school or drop out
           </p>
-          <p>
+          <p className="text-black">
             Education is a proven strategy for reducing child labour. Lack of
             access to education keeps the cycle of exploitation, illiteracy and
             poverty going – limiting future options and forcing children to
@@ -64,31 +64,35 @@ function Hero() {
           </p>
         </div>
         <div>
-          <h1 className="text-5xl font-bold text-black">Child labour is not always illegal</h1>
-          <p>
+          <h1 className="text-5xl font-bold text-black">
+            Child labour is not always illegal
+          </h1>
+          <p className="text-black">
             In many countries children working under the age of 18 is illegal –
             but not in all. Families and employers often hide what they are
             doing because they worry they will be taken to court or sent to
             prison for having child labourers.
           </p>
-          <p>
+          <p className="text-black">
             There are 46 countries who do not have laws to protect children
             under 18 from doing dangerous work.
           </p>
         </div>
         <div>
-          <h1 className="text-5xl font-bold text-black">How many child labourers are there?</h1>
-          <p>
+          <h1 className="text-5xl font-bold text-black">
+            How many child labourers are there?
+          </h1>
+          <p className="text-black">
             The International Labour Organization said in 2017 that about 152
             million children aged from five to 17 – 64 million girls and 88
             million girls – are engaged in child labour.
           </p>
-          <p>
+          <p className="text-black">
             The International Labour Organization said in 2017 that about 152
             million children aged from five to 17 – 64 million girls and 88
             million girls – are engaged in child labour.
           </p>
-          <p>
+          <p className="text-black">
             Many of them never go to school or drop out of school because they
             are required to work to earn a living for their families or have to
             work to survive themselves. The ILO said 70% work in agriculture,
@@ -96,33 +100,37 @@ function Hero() {
           </p>
         </div>
         <div>
-          <h1 className="text-5xl font-bold text-black">Why do children work?</h1>
-          <p>
+          <h1 className="text-5xl font-bold text-black">
+            Why do children work?
+          </h1>
+          <p className="text-black">
             Poverty, discrimination and marginalisation are the main drivers of
             child labour. Children in poor circumstances work because work is
             seen as the best use of their time in contributing to the needs of
             the family and preparing them for the life they are expected to
             lead.
           </p>
-          <p>
+          <p className="text-black">
             Poverty leads millions of children into paying jobs or – especially
             for young girls – domestic work that may even be unpaid but removes
             the burden of feeding and housing the child.
           </p>
-          <p>
+          <p className="text-black">
             In emergency situations – during or after conflicts or natural
             disasters – families who have had to flee from their homes often
             have to use their children to earn money just to pay for food.
           </p>
         </div>
         <div>
-          <h1 className="text-5xl font-bold text-black">What countries have child labourers?</h1>
-          <p>
+          <h1 className="text-5xl font-bold text-black">
+            What countries have child labourers?
+          </h1>
+          <p className="text-black">
             Nigeria, Pakistan and Afghanistan have the highest number of child
             labourers. They have also been affected for many years by wars and
             conflicts and have the highest numbers of children not at school.
           </p>
-          <p>
+          <p className="text-black">
             Years of conflict in Syria has seen a dramatic increase in poverty
             among Syrian families and forced more children into the labour
             force, especially as refugees face restrictions to legal work from
@@ -130,23 +138,25 @@ function Hero() {
           </p>
         </div>
         <div>
-          <h1 className="text-5xl font-bold text-black">Child labourers doing hazardous jobs</h1>
-          <p>
+          <h1 className="text-5xl font-bold text-black">
+            Child labourers doing hazardous jobs
+          </h1>
+          <p className="text-black">
             At least 85 million children do very hazardous work – forced labour,
             trafficking and bonded labour.
           </p>
-          <p>
+          <p className="text-black">
             Mining is one of the most dangerous jobs across the world but about
             one million children work in mines in appalling conditions that can
             lead to injury or death.
           </p>
-          <p>
+          <p className="text-black">
             Children also work on building sites and in brick-making factories
             without proper health and safety rules. Children are at risk from
             machinery, chemicals, harsh working conditions and lots of other
             hazards.
           </p>
-          <p>
+          <p className="text-black">
             Those who are forced to become soldiers or are slaves are treated
             very badly and suffer terrible physical and mental harm and abuse,
             as well as sexual exploitation. Child domestic workers are also at
