8622120c8a27c0a49306077e8b5bbfb1c7fe5cde
Zunaeed-Imran <zunaeede@gmail.com>
2024-12-06 07:59:35 +0600

    main - adding some text content on the hero section.

--- src/layout/Hero.jsx
+++ src/layout/Hero.jsx
@@ -4,7 +4,18 @@ function Hero() {
   return (
     <div className="bg-[#FFFFFF] pt-20">
       <div className="">
-        <h1 className="text-5xl font-bold text-black text-left pl-14">Child labour</h1>
+        <h1 className="text-5xl font-bold text-black text-left pl-14">
+          Child labour
+        </h1>
+        <div>
+          <img src="" alt="" />
+          <p>
+            This page introduces you to the issues surrounding child labour. It
+            explains what child labour is, where and why children work and how
+            working can prevent them from going to school and trap them in a
+            cycle of poverty.
+          </p>
+        </div>
       </div>
     </div>
   );
